08e10e440f1b09215fcadb618c8699a1d135ef1b
Bn161641 <60488475+Bn161641@users.noreply.github.com>
2021-11-06 21:31:15 -0500

    Login backend

--- Screens/Login.py
+++ Screens/Login.py
@@ -1,11 +1,36 @@
-from kivy.uix.widget import Widget
-
 import kivy
 from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class LoginWidget(Widget):
+    textSubmit = StringProperty("")
+    #account is a list of tuple that has username as key and password as value
+    #Dillon have the list of valid user here
+    account = [("Admin1", "Test1234"),
+             ("Admin2", "Password")]
 
 
-class LoginWindow(Widget):
-    pass
+    def onSubmit(self, subButton, userName, password):
+
+        valid = False
+
+        for x in self.account:
+            for key, value in x:
+                if(key == userName.text and value == password.text):
+                    self.valid = True;
+
+        if self.valid:
+            self.textSubmit = "Success"
+            #put transition to main menu page here
+        else:
+            self.textSubmit = "Incorrect username or password."
+
+
+
+
+
 
 class Login(App):
     pass
