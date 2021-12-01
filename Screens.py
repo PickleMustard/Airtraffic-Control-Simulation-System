2f3f6098b77d211bc6513cfc1c1aa77034d90d83
Noah Gonzales <noahagonzo@yahoo.com>
2021-11-30 19:33:39 -0600

    manual merge

--- Screens.py
+++ Screens.py
@@ -1,16 +1,18 @@
-#This code only have comments on the Login page as that is what I have wrote
-#Other comments about the code can be found on someone elses branch
-#When trying to login use "Admin1" for username and "Test1234" for the password
-#My verson only 3 buttons work on the main menu Schedule, Plane Info, and Logout
-#again, other comment about how they work can be found on a different branch (check Noah's branch)
-#he should also have implement more buttons
-from logging import NullHandler
+# Screens.py
+# Description:
+#   Contains the definitions for classes for the associated widgets in every screen
+#   This file will be split into files for each screen in the future
+
+# When trying to login use "Admin1" for username and "Test1234" for the password
+
 from kivy.metrics import dp
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.clock import Clock
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.image import Image
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
@@ -61,16 +63,14 @@ class TopBarLayout(GridLayout):
     def __init__(self, **kwargs):
         super(TopBarLayout, self).__init__(**kwargs)
 
-#this variable is so that animation does not start until a certain sceen in on
+# this variable is so that animation does not start until a certain screen in
 startMoving = False
 
-
-#this class is to change between screens
+# Class for WindowManager controller - needed to change screens
 class WindowManager(ScreenManager):
     login = ObjectProperty(None)
     mainMenu = ObjectProperty(None)
     radar = ObjectProperty(None)
-
 
 # 88""Yb    db    8888b.     db    88""Yb 
 # 88__dP   dPYb    8I  Yb   dPYb   88__dP 
@@ -78,7 +78,6 @@ class WindowManager(ScreenManager):
 # 88  Yb dP""""Yb 8888Y"  dP""""Yb 88  Yb 
 
 #this is the code for the radar page
-
 class RadarWindow(Screen):
 
     #these are the variable that corresponds with planes
@@ -215,7 +214,12 @@ class RadarWindow(Screen):
 class Plane(Widget):
     pass
 
-#this hold the code for the login page
+# 88      dP"Yb   dP""b8 88 88b 88 
+# 88     dP   Yb dP   `" 88 88Yb88 
+# 88  .o Yb   dP Yb  "88 88 88 Y88 
+# 88ood8  YbodP   YboodP 88 88  Y8 
+
+# Class for LoginWindow root widget
 class LoginWindow(Screen):
     #ObjectPropert retrieve the text input from the .kv file
     userSubmit = ObjectProperty(None)
@@ -248,14 +252,18 @@ class LoginWindow(Screen):
         else:
             self.textSubmit = "Incorrect username or password."
 
+# 8b    d8    db    88 88b 88     8b    d8 888888 88b 88 88   88 
+# 88b  d88   dPYb   88 88Yb88     88b  d88 88__   88Yb88 88   88 
+# 88YbdP88  dP__Yb  88 88 Y88     88YbdP88 88""   88 Y88 Y8   8P 
+# 88 YY 88 dP""""Yb 88 88  Y8     88 YY 88 888888 88  Y8 `YbodP' 
 
+# Class for MainMenuWindow root widget
 class MainMenuWindow(Screen):
     def changeToRadar(self):
         global startMoving
         startMoving = True
         self.manager.current = "radarScreen"
         self.manager.transition = SlideTransition(direction='right', duration=.25)
-
 
 # 88""Yb 88        db    88b 88 888888     88 88b 88 888888  dP"Yb      Yb        dP 88 88b 88 8888b.   dP"Yb  Yb        dP 
 # 88__dP 88       dPYb   88Yb88 88__       88 88Yb88 88__   dP   Yb      Yb  db  dP  88 88Yb88  8I  Yb dP   Yb  Yb  db  dP  
@@ -325,6 +333,9 @@ class PlaneInfoList(BoxLayout):
         super(PlaneInfoList, self).__init__(**kwargs)
         #Clock.schedule_once(self.finish_init,0) # do not populate at start
 
+    # Autopopulate list of plane info
+    def finish_init(self, dt):
+        self.populate()
 
     # Populate list of plane info from database
     def populate(self, plane):
@@ -385,73 +396,210 @@ class PlaneInfoList(BoxLayout):
 class ScheduleWindow(Screen):
     pass
 
-
-class PlaneInfoRow(RecycleDataViewBehavior, BoxLayout):
-    dataName = StringProperty()
-    dataValue = StringProperty()
-
+# Class for a row of ArrivalList, the list of arriving planes
+class ArrivalRow(RecycleDataViewBehavior,BoxLayout):
+    planeName = StringProperty()
+    planeArrivalTime = StringProperty()
     def __init__(self, **kwargs):
-        super(PlaneInfoRow, self).__init__(**kwargs)
+        super(ArrivalRow, self).__init__(**kwargs)
 
-
-class PlaneInfoList(BoxLayout):
+# Class for a row of DepartureList, the list of departing planes
+class DepartureRow(RecycleDataViewBehavior,BoxLayout):
+    planeName = StringProperty()
+    planeDepartureTime = StringProperty()
     def __init__(self, **kwargs):
-        super(PlaneInfoList, self).__init__(**kwargs)
-        Clock.schedule_once(self.finish_init, 0)
+        super(DepartureRow, self).__init__(**kwargs)
 
+#https://github.com/kivy/kivy/issues/6582
+# Class for the list or arriving planes
+class ArrivalList(BoxLayout):
+    def __init__(self, **kwargs):
+        super(ArrivalList, self).__init__(**kwargs)
+        Clock.schedule_once(self.finish_init,0)
+
+    # Autopopulate list of arrivals
     def finish_init(self, dt):
         self.populate()
 
-    # Dillon I need a query here for a plane's info!!!!---------------------------------------
+    # TODO:  Write query for populating list of arrivals
+    # Populate list of arrivals
     def populate(self):
-        print("Populating")
+        arrivalPlaneNames = ['Plane A', 'Plane B', 'Plane C']
+        dateTimeNow = datetime.now()
+        dateTimeA = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() + timedelta(hours=3, minutes = 27)
+        dateTimeB = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() + timedelta(hours=2, minutes = 12)
+        dateTimeC = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        arrivalPlaneDatetimes = [dateTimeA, dateTimeB, dateTimeC]
+
         self.rv.data = [
-            {'dataName': ''.join(sample(ascii_lowercase, 6)),
-             'dataValue': str(randint(0, 2000))}
-            for x in range(50)]
+            {'planeName': arrivalPlaneNames[x],
+             'planeArrivalTime': arrivalPlaneDatetimes[x]}
+            for x in range(3)]
 
-    def sort(self):
-        self.rv.data = sorted(self.rv.data, key=lambda x: x['name.text'])
+# Class for the list of departing planes
+class DepartureList(BoxLayout):
+    def __init__(self, **kwargs):
+        super(DepartureList, self).__init__(**kwargs)
+        Clock.schedule_once(self.finish_init,0)
 
-    def clear(self):
-        self.rv.data = []
+    # Autopopulate list of departures
+    def finish_init(self, dt):
+        self.populate()
 
-    def insert(self, value):
-        self.rv.data.insert(0, {
-            'name.text': value or 'default value', 'value': 'unknown'})
+    # TODO: Write query for populating list of departures
+    # Populate list of departures
+    def populate(self):
+        departurePlaneNames = ['Plane A', 'Plane B', 'Plane C']
+        dateTimeNow = datetime.now() + timedelta(hours=3, minutes = 27)
+        dateTimeA = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() + timedelta(hours=5, minutes = 27)
+        dateTimeB = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() + timedelta(hours=7, minutes = 12)
+        dateTimeC = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        departurePlaneDateteimes = [dateTimeA, dateTimeB, dateTimeC]
 
-    def update(self, value):
-        if self.rv.data:
-            self.rv.data[0]['name.text'] = value or 'default new value'
-            self.rv.refresh_from_data()
+        self.rv.data = [
+            {'planeName': departurePlaneNames[x],
+             'planeDepartureTime': departurePlaneDateteimes[x]}
+            for x in range(3)]
 
-    def remove(self):
-        if self.rv.data:
-            self.rv.data.pop(0)
+# 888888 888888 88""Yb 8b    d8 88 88b 88    db    88         .dP"Y8 88 8b    d8 
+#   88   88__   88__dP 88b  d88 88 88Yb88   dPYb   88         `Ybo." 88 88b  d88 
+#   88   88""   88"Yb  88YbdP88 88 88 Y88  dP__Yb  88  .o     o.`Y8b 88 88YbdP88 
+#   88   888888 88  Yb 88 YY 88 88 88  Y8 dP""""Yb 88ood8     8bodP' 88 88 YY 88 
 
-    # Populate the list at the start - not functioning
+# Class representing a worker in the simulation of the ground crew simulation
+class TerminalSimulatedPlane(Widget):
+    def __init__(self, **kwargs):
+        super(TerminalSimulatedPlane, self).__init__(**kwargs)
 
+# Class for the TerminalSimulationWindow root widget
+class TerminalSimulationWindow(Screen):
+    planes = []
+    def __init__(self, **kwargs):
+        super(TerminalSimulationWindow, self).__init__(**kwargs)
+        self.planes = []
+        self.initializePlanes()
 
-#    def __post_init__(self, **kwargs):
-#        super().__init__(**kwargs)
-#        PlaneInfoList
+    # Initialize simulated plane objects
+    #   Planes are initialized in an array because adding and referencing them by ids is not possible to do dynamically
+    #   Plane number and initial positions are determined by a query (not yet implemented)
+    def initializePlanes(self):
+        # Figure out the number of planes in the simulation
+        # TODO: make a query to determine number of planes
+        for x in range(0, 1):
+            newPlane = TerminalSimulatedPlane()
+            # TODO: Make a query to determine the initial position of each plane
+            newPlane.pos = (randint(200,200), randint(200,200))
+            self.planes.append(newPlane)
+            self.add_widget(self.planes[len(self.planes)-1])
 
-class ScheduleWindow(Screen):
+    # Animate the given widget. First rotate to face 
+    #   wIndex - String - The index of the widget to animate          
+    #   destX - Num - The x position to animate to
+    #   destY - Num - The y position to animate to
+    #   angleOffset - Num - The offset of the calcuated angle to animate to. 
+    #       Default if 0 offset. 
+    #       angleOffset should be 45 for GoogleAirplane.png
+    def planeAnimate(self, wIndex, destX, destY, angleOffset=0, **kwargs):
+
+        # Check that not already at destination
+        if (destX  - self.planes[wIndex].pos[0]) == 0 and (destY - self.planes[wIndex].pos[1]) == 0:
+            return
+
+        # Calculate angleOffset
+        if (destY - self.planes[wIndex].pos[1]) < 1:
+            angleOffset -= 180
+        atan = math.atan((destX  - self.planes[wIndex].pos[0]) / (destY - self.planes[wIndex].pos[1]))
+        deg = atan * 180 / math.pi
+        deg = deg * -1
+        addedOffset = deg + angleOffset
+        angle = addedOffset
+
+        # Create and start animation
+        anim = Animation(animAngle=angle, duration=.2) + Animation(x= destX, y = destY, duration=1)
+        anim.start(self.planes[wIndex])
+
+# 8b    d8    db    .dP"Y8 888888 888888 88""Yb     88      dP"Yb   dP""b8 
+# 88b  d88   dPYb   `Ybo."   88   88__   88__dP     88     dP   Yb dP   `" 
+# 88YbdP88  dP__Yb  o.`Y8b   88   88""   88"Yb      88  .o Yb   dP Yb  "88 
+# 88 YY 88 dP""""Yb 8bodP'   88   888888 88  Yb     88ood8  YbodP   YboodP 
+
+class MasterLogWindow(Screen):
     pass
 
+# Class for a row of DepartureList, the list of departing planes
+class MasterLogRow(RecycleDataViewBehavior,BoxLayout):
+    datetime = StringProperty()
+    note = StringProperty()
+    def __init__(self, **kwargs):
+        super(MasterLogRow, self).__init__(**kwargs)
 
-# https://github.com/kivy/kivy/issues/6582
-class ArrivalList(BoxLayout):
-    # Dillon I need a query here for a plane's info!!!!---------------------------------------
+class MasterLogList(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MasterLogList, self).__init__(**kwargs)
+        Clock.schedule_once(self.finish_init,0)
+
+    # Autopopulate list of departures
+    def finish_init(self, dt):
+        self.populate()
+
+    # Populate list of departures
     def populate(self):
+        dateTimeNow = datetime.now() - timedelta(hours=3, minutes = 27)
+        dateTimeA = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() - timedelta(hours=5, minutes = 27)
+        dateTimeB = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() - timedelta(hours=7, minutes = 12)
+        dateTimeC = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        datetimes = [dateTimeA, dateTimeB, dateTimeC]
+        notes = ["Plane 1 landed sucessfully", "Ground crew went on lunch", "Weather check successful"]
+
         self.rv.data = [
-            {'name.text': ''.join(sample(ascii_lowercase, 6)),
-             'value': str(randint(0, 2000))}
-            for x in range(50)]
+            {'datetime': datetimes[x],
+             'note': notes[x]}
+            for x in range(3)]
 
+# 88 88b 88  dP""b8 88 8888b.  888888 88b 88 888888     88      dP"Yb   dP""b8 
+# 88 88Yb88 dP   `" 88  8I  Yb 88__   88Yb88   88       88     dP   Yb dP   `" 
+# 88 88 Y88 Yb      88  8I  dY 88""   88 Y88   88       88  .o Yb   dP Yb  "88 
+# 88 88  Y8  YboodP 88 8888Y"  888888 88  Y8   88       88ood8  YbodP   YboodP 
 
-class DepartureList(BoxLayout):
+class IncidentLogWindow(Screen):
+    pass
+
+# Class for a row of DepartureList, the list of departing planes
+class IncidentLogRow(RecycleDataViewBehavior,BoxLayout):
+    datetime = StringProperty()
+    code = StringProperty()
+    note = StringProperty()
+    def __init__(self, **kwargs):
+        super(IncidentLogRow, self).__init__(**kwargs)
+
+class IncidentLogList(BoxLayout):
+    def __init__(self, **kwargs):
+        super(IncidentLogList, self).__init__(**kwargs)
+        Clock.schedule_once(self.finish_init,0)
+
+    # Autopopulate list of departures
+    def finish_init(self, dt):
+        self.populate()
+
+    # Populate list of departures
     def populate(self):
+        dateTimeNow = datetime.now() - timedelta(hours=100, minutes = 27)
+        dateTimeA = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() - timedelta(hours=250, minutes = 27)
+        dateTimeB = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        dateTimeNow = datetime.now() - timedelta(hours=300, minutes = 12)
+        dateTimeC = dateTimeNow.strftime('%a, %b %d, %y  -  %m-%d-%y  -  %H:%M')
+        datetimes = [dateTimeA, dateTimeB, dateTimeC]
+        codes = ['C', 'U', 'E']
+        notes = ['Collision between planes 1 and 2 on the runway', 'Unidentified plane found', 'Plane 1 emergency landing on runway 1']
+
+        self.rv.data = [
             {'datetime': datetimes[x],
              'code': codes[x],
              'note': notes[x]}
